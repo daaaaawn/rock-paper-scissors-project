1b6e09620c8c1c97847e0c032ad05a5a86f9a9ee
daaaaawn <215831470+daaaaawn@users.noreply.github.com>
2025-12-06 10:59:58 +0800

    Added player choices and playround function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3 + 1);
 
@@ -10,4 +13,58 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let humanChoice = prompt("Pick between rock, paper, scissors.").toLowerCase();
+    
+    if (humanChoice === "rock") {
+        return humanChoice;
+    } else if (humanChoice === "paper") {
+        return humanChoice;
+    } else if (humanChoice === "scissors") {
+        return humanChoice;
+    } else {
+        return "Invalid choice!";
+    }
+}
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === "rock") {
+        if (humanChoice === "rock") {
+            return `Both players chose ${computerChoice}! It's a tie!`;
+        } else if (humanChoice === "paper") {
+            humanScore++;
+            return `Human player wins!`;
+        } else if (humanChoice === "scissors") {
+            computerScore++;
+            return `Computer player wins!`;
+        }
+    } else if (computerChoice === "paper") {
+        if (humanChoice === "rock") {
+            computerScore++;
+            return `Computer player wins!`;
+        } else if (humanChoice === "paper") {
+            return `Both players chose ${computerChoice}! It's a tie!`
+        } else if (humanChoice === "scissors") {
+            humanScore++;
+            return `Human player wins!`;
+        }
+    } else if (computerChoice === "scissors") {
+        if (humanChoice === "rock") {
+            humanScore++;
+            return `Human player wins!`;
+        } else if (humanChoice === "paper") {
+            computerScore++;
+            return `Computer player wins!`;
+        } else if (humanChoice === "scissors") {
+            return `Both players chose ${computerChoice}! It's a tie!`;
+        }
+    }
+}
+
+function playGame() {
+    playRound();
+}
+
