01101a423e291a6c66f05a2dc81c8f203d1ba93b
daaaaawn <215831470+daaaaawn@users.noreply.github.com>
2025-12-06 10:05:29 +0800

    Added computer choice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3 + 1);
+
+    if (computerChoice === 1) {
+        return "rock";
+    } else if (computerChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
